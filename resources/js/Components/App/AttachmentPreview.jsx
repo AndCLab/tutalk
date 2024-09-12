b99b118a7c5c9532343a48b391d0fbced929ac0c
Andre Labendia <jaguardian00@gmail.com>
2024-09-12 17:29:02 +0800

    Adjustments to css

--- resources/js/Components/App/AttachmentPreview.jsx
+++ resources/js/Components/App/AttachmentPreview.jsx
@@ -4,16 +4,16 @@ import { formatBytes, isPDF, isPreviewable } from "@/helpers";
 
 const AttachmentPreview = ({ file }) => {
     return (
-        <div className="w-full flex items-center gap-2 py-2 px-3 rounded-md bg-slate-800">
+        <div className="w-full flex items-center gap-2 py-2 px-3 rounded-md bg-emerald-800">
             <div>
                 {isPDF(file.file) && <img src="/img/pdf.png" className="w-8" />}
                 {!isPreviewable(file.file) && (
-                    <div className="flex justify-center items-center w-10 h-10 bg-gray-700 rounded">
-                        <PaperClipIcon className="w-6" />
+                    <div className="flex justify-center items-center w-10 h-10 bg-emerald-900 rounded">
+                        <PaperClipIcon className="w-6 text-gray-100" />
                     </div>
                 )}
             </div>
-            <div className="flex-1 text-gray-400 text-nowrap text-ellipsis overflow-hidden">
+            <div className="flex-1 text-gray-300 text-nowrap text-ellipsis overflow-hidden">
                 <h3>{file.file.name}</h3>
                 <p className="text-xs">{formatBytes(file.file.size)}</p>
             </div>
