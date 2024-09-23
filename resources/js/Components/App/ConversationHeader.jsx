55bb3e6a3a704963f47b88f9b2c9deb3332644ff
Andre Labendia <jaguardian00@gmail.com>
2024-09-23 18:42:10 +0800

    Added Group Message Components

--- resources/js/Components/App/ConversationHeader.jsx
+++ resources/js/Components/App/ConversationHeader.jsx
@@ -1,9 +1,27 @@
 import { Link, usePage } from "@inertiajs/react";
-import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+import { ArrowLeftIcon, PencilSquareIcon, TrashIcon } from "@heroicons/react/24/solid";
 import UserAvatar from "./UserAvatar";
 import GroupAvatar from "./GroupAvatar";
+import GroupDescriptionPopover from "./GroupDescriptionPopover";
+import GroupUsersPopover from "./GroupUsersPopover";
+import { useEventBus } from "@/EventBus";
 
 const ConversationHeader = ({ selectedConversation }) => {
+    const authUser = usePage().props.auth.user;
+    const { emit } = useEventBus();
+
+    const onDeleteGroup = () => {
+        if(window.confirm("Are you sure you want to delete this group?")) {
+            return;
+        }
+
+        axios.delete(route("group.destroy", selectedConversation.id)).then(() => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    };
 
     return (
         <>
@@ -26,6 +44,32 @@ const ConversationHeader = ({ selectedConversation }) => {
                             )}
                         </div>
                     </div>
+                    {selectedConversation.is_group && (
+                        <div className="flex gap-3">
+                            <GroupDescriptionPopover
+                                description={selectedConversation.description}
+                            />
+                            <div className="text-gray-400">
+                            <GroupUsersPopover  
+                                users={selectedConversation.users}
+                            />
+                            </div>
+                            {selectedConversation.owner_id == authUser.id && (
+                                <>
+                                    <div className="tooltip tooltip-left z-20" data-tip="Edit Group">
+                                        <button onClick={(ev) => emit("GroupMoal.show", selectedConversation)} className="text-gray-400 hover:text-gray-200">
+                                            <PencilSquareIcon className="w-4"/>
+                                        </button>
+                                    </div>
+                                    <div className="tooltip tooltip-left z-20" data-tip="Delete Group">
+                                        <button onClick={onDeleteGroup} className="text-gray-400 hover:text-gray-200">
+                                            <TrashIcon className="w-4"/>
+                                        </button>
+                                    </div>
+                                </>
+                            )}
+                      </div>
+                    )}
                 </div>
             )}
         </>
---
+++ resources/js/Components/App/GroupModal.jsx
@@ -0,0 +1,114 @@
+import TextAreaInput from "@/Components/TextAreaInput";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import Modal from "@/Components/Modal";
+import TextInput from "@/Components/TextInput";
+import SecondaryButton from "@/Components/SecondaryButton";
+import PrimaryButton from "@/Components/PrimaryButton";
+import UserPicker from "@/Components/App/UserPicker";
+import { useForm, usePage } from "@inertiajs/react";
+import { useEventBus } from "@/EventBus";
+import { useEffect, useState } from "react";
+
+export default function GroupModal({ show = false, onClose = () => {} }) {
+    const page = usePage();
+    const conversations = page.props.conversations;
+    const { on, emit } = useEventBus();
+    const [group, setGroup] = useState({});
+    const { data, setData, processing, reset, post, put, errors } = useForm({
+        id: "",
+        name: "",
+        description: "",
+        user_ids: [],
+    });
+    const users = conversations.filter((c) => !c.is_group);
+
+    const createOrUpdateGroup = (e) => {
+        e.preventDefault();
+        if (group.id) {
+            put(route("group.update", group_id), {
+                onSuccess: () => {
+                    closeModal();
+                    emit("toast.show", `Group "${data.name}" was updated`);
+                },
+            });
+            return;
+        }
+        post(route("group.store"), {
+            onSuccess: () => {
+                emit("toast.show", `Group "${data.name}" was created`);
+                closeModal();
+            },
+        });
+    };
+
+    const closeModal = () => {
+        reset();
+        onClose();
+    };
+
+    useEffect(() => {
+        return on("GroupModal.show", (group) => {
+            setData({
+                name: group.name,
+                description: group.description,
+                user_ids: group.users
+                    .filter((u) => group.owner_id !== u.id)
+                    .map((u) => u.id),
+            });
+            setGroup(group);
+        });
+    }, [on]);
+
+    return (
+        <Modal show={show} onClose={closeModal}>
+            <form onSubmit={createOrUpdateGroup} className="p-6 overflow-y-auto">
+                <h2 className="text-xl font-medium text-gray-900 dark:text-emerald-800">
+                    {group.id ? `Edit Group "${group.name}"` : "Create new Group"}
+                </h2>
+                <div className="mt-8">
+                    <InputLabel htmlFor="name" value="Name" />
+                    <TextInput 
+                        id="name" 
+                        className="mt-1 block w-full" 
+                        value={data.name} 
+                        disabled={!!group.id} 
+                        onChange={(e) => setData("name", e.target.value)}
+                        required
+                    />
+                    <InputError className="mt-2" message={errors.description} />
+                </div>
+
+                <div className="mt-4">
+                    <InputLabel value="Select Users" />
+                    <UserPicker 
+                        value={
+                            users.filter(
+                                (u) => 
+                                    group.owner_id !== u.id && 
+                                    data.user_ids.includes(u.id)
+                            ) || []
+                        }
+                        options={users}
+                        onSelect={(users) =>
+                            setData(
+                                "user_ids", 
+                                users.map((u) => u.id)
+                            )
+                        }
+                    />
+                    <InputError className="mt-2" message={errors.user_ids} />
+                </div>
+
+                <div className="mt-6 flex justify-end">
+                    <SecondaryButton onClick={closeModal}>
+                        Cancel
+                    </SecondaryButton>
+                    <PrimaryButton className="ms-3" disabled={processing}>
+                        {group.id ? "Update" : "Create"}
+                    </PrimaryButton>
+                </div>
+            </form>
+        </Modal>
+    );
+}
